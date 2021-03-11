e609a4c14c22113e858684282277fed8b405abdc
sweetpotato27 <matthewsdb34@gmail.com>
2021-03-10 21:36:36 -0800

    landing transition to nav heading

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import Portrait from './portfolio-portrait.png'
 
 
 function landing() {
+    const main = document.getElementById('main');
     const div = document.createElement('div');
     const portrait = document.createElement('img');
     div.setAttribute("id", "portrait-div");
     portrait.setAttribute("id", "portrait");
     portrait.src = Portrait;
     div.appendChild(portrait);
-    div.appendChild(sayHello());
+    main.appendChild(sayHello());
     
     return div;
 }
@@ -22,7 +23,11 @@ function sayHello() {
     const h1 = document.createElement('h1');
     const greeting = document.createElement('span');
     const name = document.createElement('span');
-    h1.classList.add('greeting');
+    div.setAttribute('id', 'greeting-div');
+    h1.setAttribute('id', 'heading');
+    h1.classList.add("land-heading");
+    greeting.setAttribute('id', 'greeting');
+    name.setAttribute('id', 'name');
     h1.appendChild(greeting);
     h1.appendChild(name);
     greeting.innerHTML = "";
@@ -36,12 +41,12 @@ function sayHello() {
             .promise()
             .done(() => {
                 if (i === phrase.length && bool) {
-                    // next animation here
+                    // name animation happens after greeting
                     typePhrase(name, "Dylan Matthews", 100, 0, false);
                 }
                 if (i === phrase.length && !bool) {
-                    console.log("end");
-                    //trigger some event here
+                    // now transition from the landing page to the main site look
+                    landingMainTransition();
                 }
                 if( i < phrase.length ) {
                     i++;
@@ -53,8 +58,35 @@ function sayHello() {
     return div;
 }
 
+function landingMainTransition() {
+    console.log("transitioning....");
+    const nav = document.createElement('nav');
+    nav.setAttribute('id', 'nav');
+    const portrait = document.getElementById('portrait-div');
+    const greetDiv = document.getElementById('greeting-div');
+    const h1 = document.getElementById('heading');
+    const greeting = document.getElementById('greeting');
+    const name = document.getElementById('name');
+    $(nav).insertBefore($("#main"));
+    $(portrait).fadeOut();
+    $(greeting).fadeTo(1000, 0.0000001, () => {
+        $(h1).addClass("land-heading-transition");
+        setTimeout(() => {
+            $(greetDiv).remove();
+            $(greeting).remove();
+            $(h1).removeClass();
+            $(h1).addClass('nav-heading');
+            $(nav).append(h1);
+        }, 1100)
+    });
+}
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.body.appendChild(landing());
+    const main = document.createElement('div');
+    main.setAttribute('id', 'main');
+    document.body.appendChild(main);
+
+    main.appendChild(landing());
 });
